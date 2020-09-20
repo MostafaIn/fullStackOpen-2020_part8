bb4837a04016df914da93509e22ad8ef1615bfc3
mostafaIn <mostafa2015se@gmail.com>
2020-09-20 23:29:52 +0300

    8.19 Books by genre, part 1

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from './queries'
 
 
 const Books = (props) => {
+  const [genres, setGenres] = useState([])
+  const [genre, setGenre] = useState("")
+  const [booksData, setBooksData] = useState([])
   const result = useQuery(ALL_BOOKS)
+
+  useEffect(() => {
+    if(result.data){
+      setBooksData(result.data.allBooks)
+      const genresArr = result.data.allBooks.flatMap(b => b.genres)
+      setGenres([...new Set(genresArr), "All genres"])
+    }
+  }, [result])
+
+  const filteredByGenre = booksData.filter( book => (genre && genre !== "All genres") ? book.genres.includes(genre) : book)
 
   if (!props.show) {
     return null
-  }
+  } 
+  console.log(genre);
+  console.log(filteredByGenre);
+  
   return (
     <div>
       <h2>books</h2>
@@ -24,7 +40,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {result.data.allBooks.map((a, i) =>
+          {filteredByGenre.map((a, i) =>
             <tr key={i}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -33,6 +49,14 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      <div style={{marginTop: '2rem'}}>
+          {genres.map( (genre, i) => <button 
+            key={i} 
+            onClick={() => setGenre(genre)}>
+            {genre}
+            </button>
+          )}
+      </div>
     </div>
   )
 }
